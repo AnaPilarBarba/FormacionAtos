095595720dd1d8fefb8a8c0ade328559c4e68da2
Ana Pilar Barba (ext) <ana.barba.external@atos.net>
2023-03-28 10:12:47 +0200

    Se ha realizado un proyecto con exito sobre la subida de fichero desde las imagenes que tenemos en nuestra galeria

---
+++ SubidaMultimedia/Actividad9/app/src/main/java/com/example/actividad9/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.actividad9;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageView = findViewById(R.id.imageCamera);
+    }
+
+    public void onClick(View view) {
+        cargarImagen();
+    }
+
+    private void cargarImagen(){
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        intent.setType("image/");
+        startActivityForResult(intent.createChooser(intent, "Selecione la aplicacion"),10);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK){
+            Uri path = data.getData();
+            imageView.setImageURI(path);
+        }
+    }
+}
